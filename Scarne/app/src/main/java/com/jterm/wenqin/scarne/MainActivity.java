c88cab6ef10bf6f9bd2f158df000538494b78aa6
Wenqin <wchen15@smith.edu>
2017-01-05 19:30:29 -0800

    Bugs remain on Scarne

--- Scarne/app/src/main/java/com/jterm/wenqin/scarne/MainActivity.java
+++ Scarne/app/src/main/java/com/jterm/wenqin/scarne/MainActivity.java
@@ -1,65 +1,100 @@
 package com.jterm.wenqin.scarne;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.Random;
+
 
 public class MainActivity extends AppCompatActivity {
     public int userOverScore=0;
     public int userTurnScore=0;
     public int computerOverScore = 0;
     public int computerTurnScore=0;
+    public boolean userTurn=true;
     Random rand=new Random();
+    private static final String TAG = MainActivity.class.getSimpleName();
 
-    public void ScoreDisplay(){
+    public void OverScoreDisplay(){
         userOverScore+=userTurnScore;
         computerOverScore+=computerTurnScore;
         TextView labelText = (TextView) findViewById(R.id.text1);
-        labelText.setText("Your score: "+userOverScore+" Computer score:"+computerOverScore);
+        labelText.setText("Your overall score: "+userOverScore+" Computer overall score: "+computerOverScore);
     }
 
+    public void TurnScoreDisplay(){
+        TextView labelText = (TextView) findViewById(R.id.text2);
+        labelText.setText("Your turn score: "+userTurnScore+" Computer turn score: "+computerTurnScore);
+    }
+    public void userRoll(View view){
+        rollDice();
+        }
 
-
-    public void Roll(View view){
+    public void diceDisplay(ImageView diceImage, int n){
+        switch(n){
+            case 1:
+                Log.d(TAG, "Display dice1 here");
+                diceImage.setImageDrawable(getResources().getDrawable(R.drawable.dice1));
+                break;
+            case 2:
+                diceImage.setImageDrawable(getResources().getDrawable(R.drawable.dice2));
+                break;
+            case 3:
+                diceImage.setImageDrawable(getResources().getDrawable(R.drawable.dice3));
+                break;
+            case 4:
+                diceImage.setImageDrawable(getResources().getDrawable(R.drawable.dice4));
+                break;
+            case 5:
+                diceImage.setImageDrawable(getResources().getDrawable(R.drawable.dice5));
+                break;
+            case 6:
+                diceImage.setImageDrawable(getResources().getDrawable(R.drawable.dice6));
+                break;
+        }
+    }
+    public void rollDice(){
+        Log.d(TAG, "Dice rolled here");
         int n = rand.nextInt(6) + 1;
 
-        if (n==1){
-            userOverScore = 0;
-            ImageView diceImage = (ImageView)findViewById(R.id.diceImage);
-            diceImage.setImageDrawable(getResources().getDrawable(R.drawable.dice1));
-            ScoreDisplay();
-        }
-        else{
-            userTurnScore += n;
-            ScoreDisplay();
-            switch(n){
-                case 2:
-                    ImageView diceImage = (ImageView)findViewById(R.id.diceImage);
-                    diceImage.setImageDrawable(getResources().getDrawable(R.drawable.dice2));
-
-                    break;
-                case 3:
-                    diceImage = (ImageView)findViewById(R.id.diceImage);
-                    diceImage.setImageDrawable(getResources().getDrawable(R.drawable.dice3));
-                    break;
-                case 4:
-                    diceImage = (ImageView)findViewById(R.id.diceImage);
-                    diceImage.setImageDrawable(getResources().getDrawable(R.drawable.dice4));
-                    break;
-                case 5:
-                    diceImage = (ImageView)findViewById(R.id.diceImage);
-                    diceImage.setImageDrawable(getResources().getDrawable(R.drawable.dice5));
-                    break;
-                case 6:
-                    diceImage = (ImageView)findViewById(R.id.diceImage);
-                    diceImage.setImageDrawable(getResources().getDrawable(R.drawable.dice6));
-                    break;
+        if (userTurn==true){
+            if (n==1){
+                userTurnScore=0;
+                userOverScore=0;
+            }
+            else{
+                userTurnScore += n;
             }
         }
+        //computer's turn:
+        else{
+            if (n==1){
+                computerTurnScore=0;
+                computerOverScore = 0;
+            }
+            else{
+                computerTurnScore += n;
+            }
+        }
+
+        ImageView diceImage = (ImageView)findViewById(R.id.diceImage);
+        if (n==1){
+            Log.d("UpdateTurnScore" , String.valueOf(userTurnScore));
+            TurnScoreDisplay();
+            diceDisplay(diceImage,n);
+            holdDice();
+        }
+        else{
+            TurnScoreDisplay();
+            diceDisplay(diceImage,n);
+        }
+
     }
 
     public void Reset(View view){
@@ -67,19 +102,55 @@ public class MainActivity extends AppCompatActivity {
         userTurnScore=0;
         computerTurnScore=0;
         computerOverScore=0;
-        ScoreDisplay();
+        TurnScoreDisplay();
+        OverScoreDisplay();
     }
 
-    public void Hold(View view){
-        ScoreDisplay();
-        userTurnScore=0;
-        
+    public void userHold(View view){
+
+        holdDice();
+    }
+
+    public void holdDice(){
+        Log.d(TAG, "Dice held here");
+        OverScoreDisplay();
+        userTurn = !userTurn;
+        Button rollButton = (Button)findViewById(R.id.rollButton);
+        rollButton.setEnabled(userTurn);
+        Button holdButton = (Button)findViewById(R.id.holdButton);
+        holdButton.setEnabled(userTurn);
+        Log.d("userTurn", String.valueOf(userTurn));
+
+        if (userTurn==true) {
+
+            Log.d(TAG, "User turn switched on");
+        }
+        else {
+            Log.d(TAG, "User turn switched off");
+            computerTurn();
+            Log.d(TAG, "User turn switched off");
+        }
+
     }
 
 
     public void computerTurn(){
-        while(computerTurnScore<20){
+        final Handler mHandler = new Handler();
+        final Runnable mRunnable = new Runnable(){
+            @Override
+            public void run() {
+                rollDice();
+                mHandler.postDelayed(this, 1000);
+            }
+        };
 
+        while (computerTurnScore<20){
+            mHandler.postDelayed(mRunnable, 1000);
+        }
+
+        mHandler.removeCallbacks(mRunnable);
+        if (computerTurnScore >= 20){
+            holdDice();
         }
     }
 
